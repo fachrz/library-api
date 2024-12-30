db2e8e29a9476cc546a126f51cc993554b139e30
fachrz <rfachru3@outlook.com>
2024-12-30 08:13:27 +0700

    first commit

---
+++ src/main/groovy/com/example/library_api/LibraryApiApplication.groovy
@@ -0,0 +1,13 @@
+package com.example.library_api
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class LibraryApiApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(LibraryApiApplication, args)
+	}
+
+}
